05e98ca43814f70e9084079ff3dc27b1a28f63b3
KhamzaKuranbayev <khamza@nightwell-logistics.com>
2022-05-18 22:03:19 +0500

    User service implemented but not finished (v1)

--- src/apptest/repository/UserRepository.java
+++ src/apptest/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package apptest.repository;
 
-public interface UserRepository {
+import apptest.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    public static List<User> userList = new ArrayList<>();
+
+    public static List<User> getUserList() {
+        return userList;
+    }
+
+    public static void setUserList(List<User> userList) {
+        UserRepository.userList = userList;
+    }
 }
--- src/apptest/service/impl/UserServiceImpl.java
+++ src/apptest/service/impl/UserServiceImpl.java
@@ -1,2 +1,12 @@
-package apptest.service.impl;public class UserServiceImpl {
+package apptest.service.impl;
+
+import apptest.model.User;
+import apptest.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void save(User user) {
+        // TODO: 5/18/2022 SAVE NEW USER TO REPOSITORY
+    }
 }
